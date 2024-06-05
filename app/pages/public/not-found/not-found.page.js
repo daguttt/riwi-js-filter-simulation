10b739e569d8e791463c77768e47ed79a10dd8ce
daguttt <dagutmu667@gmail.com>
2024-06-05 00:40:32 -0500

    add content to not found page

--- app/pages/public/not-found/not-found.page.js
+++ app/pages/public/not-found/not-found.page.js
@@ -1,3 +1,7 @@
 export function NotFoundPageComponent() {
+  const $root = document.getElementById('root');
+  $root.innerHTML = /*html*/ `
+    <h1>error 404 <h1>
+  `;
   console.log('Mostrando Not Found');
 }
